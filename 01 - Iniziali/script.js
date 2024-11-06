a2596d79aa3e991af52856eac455ad8b2b57cbc7
mbaisotti99 <matteo.baisotti@gmail.com>
2024-11-06 16:53:46 +0100

    1+2

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -6,7 +6,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 // Dichiara la funzione qui.
 
 
+getInitials = (array) => {
+    
+    let initials = [];
+    for (i = 0; i < array.length; i++){
+        initials[i] = array[i].slice(0,1); 
+    }
+    return initials;
+}
+
 // Invoca la funzione qui e stampa il risultato in console
+
+const init = getInitials(names);
+
+console.log(init);
 
 
 
--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -5,8 +5,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+getLetter = (init, array) => {
+    let word = [];
+    for (let i = 0; i < array.length; i++)
+        {
+            if (array[i].includes(init))
+                {
+                    word.push(array[i])
+                }
+        }
+    return word;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+
+const result = getLetter("A", names)
+console.log(result);
 
 
 
