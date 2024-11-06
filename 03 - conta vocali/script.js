4cebdd998870450f21773dd8f0925663e173ceab
mbaisotti99 <matteo.baisotti@gmail.com>
2024-11-06 17:55:35 +0100

    madonna che sclero questo

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,25 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+getVowels = (string) =>
+    {
+        let vowelsNumb = 0;
+        const vowels = ["a", "e", "i", "o", "u"]
+        letters = string.split('')
+        for (let i = 0; i < letters.length; i++){
+            for (let j = 0; j < vowels.length; j++){
+                if (letters[i] === vowels[j]){
+                    vowelsNumb++
+                } 
+            }
+        }
+        return vowelsNumb; 
+    }
 
 // Invoca la funzione qui e stampa il risultato in console
 
+let result = getVowels(word);
+console.log(result);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
